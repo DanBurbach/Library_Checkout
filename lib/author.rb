a3e61db1a3956f001a2d05b3e7875652bd7f8439
Dan Burbach and Ngan Nguyen <ngan@>
2018-12-05 11:44:52 -0800

    passing tests for second class test group

--- lib/author.rb
+++ lib/author.rb
@@ -4,50 +4,47 @@ require'pg'
 DB = PG.connect({:dbname => 'library'})
 
 class Author
-  attr_accessor(:first_name, :last_name, :specialty_id)
+  attr_accessor(:name, :books)
   attr_reader(:id)
 
   def initialize(attributes)
-    @first_name = attributes.fetch(:first_name)
-    @last_name = attributes.fetch(:last_name)
-    @specialty_id = attributes.fetch(:specialty_id)
+    @name = attributes.fetch(:name)
+    @books = attributes.fetch(:books)
     @id = attributes.fetch(:id).to_i rescue nil
   end
 
   def self.all
-    returned_doctors = DB.exec("SELECT * FROM doctors_tb;")
-    doctors = []
-    returned_doctors.each() do |doctor|
-      first_name = doctor.fetch("first_name")
-      last_name = doctor.fetch("last_name")
-      specialty_id = doctor.fetch("specialty_id").to_i
-      id = doctor.fetch("id").to_i()
-      doctors.push(Doctor.new({:first_name => first_name, :last_name => last_name, :specialty_id => specialty_id, :id => id}))
+    returned_authors = DB.exec("SELECT * FROM author;")
+    authors = []
+    returned_authors.each() do |author|
+      name = author.fetch("name")
+      books = author.fetch("books")
+      id = author.fetch("id").to_i()
+      authors.push(Author.new({:name => name, :books => books, :id => id}))
     end
-    doctors
+    authors
   end
 
   def self.find(id)
-    returned_doctors = DB.exec("SELECT * FROM doctors_tb WHERE id = #{id};")
-    returned_doctors.each() do |doctor|
-      first_name = doctor.fetch("first_name")
-      last_name = doctor.fetch("last_name")
-      specialty_id = doctor.fetch("specialty_id").to_i
-      id = doctor.fetch("id").to_i()
-      return Doctor.new({:first_name => first_name, :last_name => last_name, :specialty_id => specialty_id, :id => id})
+    returned_authors = DB.exec("SELECT * FROM author WHERE id = #{id};")
+    returned_authors.each() do |author|
+      name = author.fetch("name")
+      books = author.fetch("books")
+      id = author.fetch("id").to_i()
+      return Author.new({:name => name, :books => books, :id => id})
     end
   end
 
   def save
-    result = DB.exec("INSERT INTO doctors_tb(first_name, last_name, specialty_id) VALUES ('#{@first_name}','#{@last_name}', #{specialty_id}) RETURNING id;")
+    result = DB.exec("INSERT INTO author(name, books, id) VALUES ('#{@name}','#{@books}', #{@id}) RETURNING id;")
     @id = result.first().fetch("id").to_i()
   end
 
-  def ==(another_doctor)
-    self.first_name().==(another_doctor.first_name()).&(self.last_name().==(another_doctor.last_name())).&(self.specialty_id().==(another_doctor.specialty_id()))
+  def ==(another_author)
+    self.name().==(another_author.name()).&(self.books().==(another_author.books()))
   end
 
   def Delete(id)
-    DB.exec("DELETE FROM doctors_tb WHERE id = #{doctor.id};")
+    DB.exec("DELETE FROM authors_tb WHERE id = #{author.id};")
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,40 +1,40 @@
-# require('spec_helper')
-#
-# describe(Author) do
-#   describe(".all") do
-#     it("starts off with no lists") do
-#       expect(List.all()).to(eq([]))
-#     end
-#   end
-#
-#   describe("#name") do
-#     it("tells you its name") do
-#       list = List.new({:name => "Epicodus stuff", :id => nil})
-#       expect(list.name()).to(eq("Epicodus stuff"))
-#     end
-#   end
-#
-#   describe("#id") do
-#     it("sets its ID when you save it") do
-#       list = List.new({:name => "Epicodus stuff", :id => nil})
-#       list.save()
-#       expect(list.id()).to(be_an_instance_of(Integer))
-#     end
-#   end
-#
-#   describe("#save") do
-#     it("lets you save lists to the database") do
-#       list = List.new({:name => "Epicodus stuff", :id => nil})
-#       list.save()
-#       expect(List.all()).to(eq([list]))
-#     end
-#   end
-#
-#   describe("#==") do
-#     it("is the same list if it has the same name") do
-#       list1 = List.new({:name => "Epicodus stuff", :id => nil})
-#       list2 = List.new({:name => "Epicodus stuff", :id => nil})
-#       expect(list1).to(eq(list2))
-#     end
-#   end
-# end
+require('spec_helper')
+
+describe(Author) do
+  describe(".all") do
+    it("starts off with no lists") do
+      expect(Author.all()).to(eq([]))
+    end
+  end
+
+  describe("#name") do
+    it("tells you its name") do
+      author = Author.new({:name => "Epicodus stuff", :books => "Funsies", :id => nil})
+      expect(author.name()).to(eq("Epicodus stuff"))
+    end
+  end
+
+  describe("#id") do
+    it("sets its ID when you save it") do
+      author = Author.new({:name => "Epicodus stuff", :books => "Funsies", :id => nil})
+      author.save()
+      expect(author.id()).to(be_an_instance_of(Integer))
+    end
+  end
+
+  describe("#save") do
+    it("lets you save authors to the database") do
+      author = Author.new({:name => "Epicodus stuff", :books => "Funsies", :id => nil})
+      author.save()
+      expect(Author.all()).to(eq([author]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same author if it has the same name") do
+      author1 = Author.new({:name => "Epicodus stuff", :books => "Funsies", :id => nil})
+      author2 = Author.new({:name => "Epicodus stuff", :books => "Funsies", :id => nil})
+      expect(author1).to(eq(author2))
+    end
+  end
+end
